6a75d59e7e39dbb8f9269da1599bb94b3464ad75
verdipratama <verdipratama@icloud.com>
2019-12-02 08:17:34 +0700

    100% done

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,65 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello from Firebase!');
+});
+
+/**
+|--------------------------------------------------
+| ADD NOTIFICATIONS TRIGGERS
+|--------------------------------------------------
+*/
+
+const createNotification = notification => {
+  return admin
+    .firestore()
+    .collection('notifications')
+    .add(notification)
+    .then(doc => console.log('notifications added', doc));
+};
+
+exports.projectCreated = functions.firestore
+  // Foe Projects
+  .document('projects/{projectId}')
+  .onCreate(doc => {
+    const project = doc.data();
+    const notification = {
+      content: 'Added a new project',
+      user: `${project.authorFirstName} ${project.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    };
+
+    return createNotification(notification);
+  });
+
+/**
+|--------------------------------------------------
+| Auth Trigger for New User
+|--------------------------------------------------
+*/
+
+exports.userJoined = functions.auth
+  // Auth
+  .user()
+  .onCreate(user => {
+    return admin
+      .firestore()
+      .collection('users')
+      .doc(user.uid)
+      .get()
+      .then(doc => {
+        const newUser = doc.data();
+        const notification = {
+          content: 'Joined the party',
+          user: `${newUser.firstName} ${newUser.lastName}`,
+          time: admin.firestore.FieldValue.serverTimestamp()
+        };
+
+        return createNotification(notification);
+      });
+  });
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -11,7 +11,7 @@ import Routing from '../auth/Routing';
 class Dashboard extends Component {
   render() {
     console.log(this.props);
-    const { tampilProject } = this.props;
+    const { tampilProject, notif } = this.props;
 
     return (
       <Routing>
@@ -21,7 +21,7 @@ class Dashboard extends Component {
               <ProjectList projectlist={tampilProject} />
             </div>
             <div className="col s12 m5 offset-m1">
-              <Notifications />
+              <Notifications notifications={notif} />
             </div>
           </div>
         </div>
@@ -37,14 +37,18 @@ const mapStateToProps = state => {
     // projects dari property array projectState di reducer.js
     // tampilProject: state.project.projects
     // firestore property dari index.js store
-    tampilProject: state.firestore.ordered.projects
+    tampilProject: state.firestore.ordered.projects,
+    notif: state.firestore.ordered.notifications
   };
 };
 
 // export default connect(mapStateToProps)(Dashboard);
 export default compose(
   connect(mapStateToProps),
-  firestoreConnect([{ collection: 'projects', orderBy: ['createdAt', 'desc'] }])
+  firestoreConnect([
+    { collection: 'projects', orderBy: ['createdAt', 'desc'] },
+    { collection: 'notifications', limit: 3, orderBy: ['time', 'desc'] }
+  ])
 )(Dashboard);
 
 // export default compose(firestoreConnect(['projects']), connect(mapStateToProps))(Dashboard);
